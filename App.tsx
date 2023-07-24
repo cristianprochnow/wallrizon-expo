193e936abdf12c074783d774afc147f56ac54487
Cristian Prochnow <cristianprochnow@gmail.com>
2023-07-24 12:05:06 -0300

    feat: add initial styles

--- App.tsx
+++ App.tsx
@@ -1,15 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Home from './src/screens/Home';
+import { colors } from './src/constants/theme';
 
 export default function App() {
   return (
-    <>
+    <View style={styles.container}>
       <StatusBar style="light" />
       <SafeAreaView>
         <Home />
       </SafeAreaView>
-    </>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background, 
+    flex: 1
+  }
+});
